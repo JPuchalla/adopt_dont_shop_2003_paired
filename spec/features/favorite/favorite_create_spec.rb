076a4c49119f9d487080389ceb2e788ea731c270
Alex Pariseau <arpariseau@gmail.com>
2020-05-13 17:39:45 -0600

    Has a button to make a pet a favorite

---
+++ spec/features/favorite/favorite_create_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.describe "Marking pet as favorite", type: :feature do
+  before :each do
+    shelter_1 = Shelter.create(name: "Dumb Friends League",
+                               address: "2080 S. Quebec St.",
+                               city: "Denver",
+                               state: "CO",
+                               zip: "80231")
+
+    @cassidy = Pet.create(image: "cute_dog.jpg",
+                       name: "Cassidy",
+                       description: "A very adorable pupper.",
+                       approx_age: 10,
+                       sex: "female",
+                       adopt_status: 'pending',
+                       shelter_id: shelter_1.id,
+                       favorite: false)
+
+    visit "/pets/#{@cassidy.id}"
+  end
+
+  it "can favorite a pet" do
+    expect(page).to have_content("Favorited Pets: 0")
+    click_button "Favorite Pet"
+
+    expect(current_path).to eq("/pets/#{@cassidy.id}")
+    expect(page).to have_content("You have favorited #{@cassidy.name}.")
+    expect(page).to have_content("Favorited Pets: 1")
+    expect(@cassidy.favorite).to eq(true)
+  end
+
+end
