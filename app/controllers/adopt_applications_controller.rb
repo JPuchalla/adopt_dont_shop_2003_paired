7b137da3dd8baf11a298fefa21e28d3ee6dd479f
Alex Pariseau <arpariseau@gmail.com>
2020-05-16 14:10:20 -0600

    Refactor, simplifying model

--- app/controllers/adopt_applications_controller.rb
+++ app/controllers/adopt_applications_controller.rb
@@ -6,9 +6,10 @@ class AdoptApplicationsController < ApplicationController
   def create
     applicant = AdoptApplication.create(application_params)
     params[:pet_ids].each do |pet_id|
-      applicant.apply_for_pet(pet_id)
+      PetApplication.create(pet_id: pet_id, adopt_application_id: applicant.id)
       favorite.remove_fav(pet_id)
     end
+    session[:favorite] = favorite.contents
     flash[:notice] = "Application submitted!"
     redirect_to "/favorites"
   end
--- app/models/adopt_application.rb
+++ app/models/adopt_application.rb
@@ -3,8 +3,4 @@ class AdoptApplication < ApplicationRecord
                         :zip, :phone, :description
   has_many :pet_applications
   has_many :pets, through: :pet_applications
-
-  def apply_for_pet(pet_id)
-      PetApplication.create(pet_id: pet_id, adopt_application_id: id)
-  end
 end
