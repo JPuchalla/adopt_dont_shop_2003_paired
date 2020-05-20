d3259c50cdb6dba5c9d0765170c14ef5033559d1
Alex Pariseau <arpariseau@gmail.com>
2020-05-20 13:57:15 -0600

    Add stats for shelter on the shelter's show page

--- spec/features/shelter/shelter_read_spec.rb
+++ spec/features/shelter/shelter_read_spec.rb
@@ -34,4 +34,50 @@ RSpec.describe "Shelter read page", type: :feature do
     expect(current_path).to eq("/pets")
   end
 
+  it "has statistics for the shelter" do
+    cassidy = Pet.create(image: "cute_dog.jpg",
+                       name: "Cassidy",
+                       description: "A very adorable pupper.",
+                       approx_age: 10,
+                       sex: "female",
+                       adopt_status: 'adoptable',
+                       shelter_id: @shelter_1.id)
+
+    hobbes = Pet.create(image: "smug_cat.jpg",
+                       name: "Hobbes",
+                       description: "A very mischievous cat.",
+                       approx_age: 5,
+                       sex: "male",
+                       adopt_status: 'adoptable',
+                       shelter_id: @shelter_1.id)
+
+    review_1 = Review.create(title: "Awesome place!",
+                              rating: 5,
+                              content: "Truly enjoyed our time working with this shelter. Staff was great, and we found our perfect pet!",
+                              image: nil,
+                              shelter_id: @shelter_1.id)
+
+    review_2 = Review.create(title: "Found the one",
+                              rating: 4,
+                              content: "It took us a bit of time, but we found that special little guy to bring home to the kids!",
+                              image: nil,
+                              shelter_id: @shelter_1.id)
+
+    francis = AdoptApplication.create(name: "Francis",
+                               address: "2080 S. Quebec St.",
+                               city: "Denver",
+                               state: "CO",
+                               zip: "80231",
+                               phone: "2023332291",
+                               description: "I am a perfect human.")
+
+    PetApplication.create(pet_id: cassidy.id, adopt_application_id: francis.id, approval: false)
+    PetApplication.create(pet_id: hobbes.id, adopt_application_id: francis.id, approval: false)
+
+    visit "/shelters/#{@shelter_1.id}"
+    expect(page).to have_content("Number of pets at this shelter: 2")
+    expect(page).to have_content("Average rating of this shelter: 4.5")
+    expect(page).to have_content("Applications on file at this shelter: 1")
+  end
+
 end
